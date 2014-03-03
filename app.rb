683779677045ab47e6d9853c6402b0514d75546f
Larry Botha <larry@fixate.it>
2014-03-03 15:03:43 +0200

    initial commit

---
+++ app.rb
@@ -0,0 +1,88 @@
+require 'sinatra'
+require 'kss'
+
+get '/' do
+  erb :index
+end
+
+get '/styleguide' do
+  @styleguide = Kss::Parser.new('public/stylesheets')
+  erb :styleguide
+end
+
+get '/alerts' do
+  @styleguide = Kss::Parser.new('public/stylesheets')
+  erb :alerts
+end
+
+get '/base' do
+  @styleguide = Kss::Parser.new('public/stylesheets')
+  erb :base
+end
+
+get '/buttons' do
+  @styleguide = Kss::Parser.new('public/stylesheets')
+  erb :buttons
+end
+
+get '/color' do
+  @styleguide = Kss::Parser.new('public/stylesheets')
+  erb :color
+end
+
+get '/forms' do
+  @styleguide = Kss::Parser.new('public/stylesheets')
+  erb :forms
+end
+
+get '/gallery' do
+  @styleguide = Kss::Parser.new('public/stylesheets')
+  erb :gallery
+end
+
+get '/grid' do
+  @styleguide = Kss::Parser.new('public/stylesheets')
+  erb :grid
+end
+
+get '/nav' do
+  @styleguide = Kss::Parser.new('public/stylesheets')
+  erb :nav
+end
+
+get '/progress-bars' do
+  @styleguide = Kss::Parser.new('public/stylesheets')
+  erb :progress_bars
+end
+
+get '/text-variations' do
+  @styleguide = Kss::Parser.new('public/stylesheets')
+  erb :text_variations
+end
+
+get '/tool-tips' do
+  @styleguide = Kss::Parser.new('public/stylesheets')
+  erb :tool_tips
+end
+
+helpers do
+  # Generates a styleguide block. A little bit evil with @_out_buf, but
+  # if you're using something like Rails, you can write a much cleaner helper
+  # very easily.
+  def styleguide_block(section, &block)
+    @section = @styleguide.section(section)
+    @example_html = capture{ block.call }
+    @escaped_html = ERB::Util.html_escape @example_html
+    @_out_buf << erb(:_styleguide_block)
+  end
+
+  # Captures the result of a block within an erb template without spitting it
+  # to the output buffer.
+  def capture(&block)
+    out, @_out_buf = @_out_buf, ""
+    yield
+    @_out_buf
+  ensure
+    @_out_buf = out
+  end
+end
